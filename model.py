a75a785099ef75c8ef60770b78237230f38bcb86
CodeKnight314 <rtang24@nmhschool.org>
2024-10-24 22:27:11 +0100

    Create RenatusV2

--- model.py
+++ model.py
@@ -23,8 +23,8 @@ class ResidualBlock(nn.Module):
         x = residual + x
         return x
 
-class Renatus(nn.Module): 
-    def __init__(self, input_channels: int, num_blocks: int):
+class RenatusV1(nn.Module): 
+    def __init__(self, input_channels: int = 12, num_blocks: int = 8):
         super().__init__()
         
         self.conv_one = nn.Conv2d(input_channels, out_channels=64, kernel_size=3, stride=1, padding=1)
@@ -53,6 +53,42 @@ class Renatus(nn.Module):
         x = self.q_layers(x)
         
         return x
+
+class RenatusV2(nn.Module):
+    def __init__(self, input_channels: int = 27, num_blocks: int = 19):
+        super().__init__()
+
+        self.conv_one = nn.Conv2d(input_channels, 128, kernel_size=3, stride=1, padding=1)
+        self.batch_one = nn.BatchNorm2d(128)
+        
+        self.res_stack = nn.Sequential(*[ResidualBlock(128, 128) for _ in range(num_blocks)])
+        
+        self.policy_conv = nn.Conv2d(128, 2, kernel_size=1)
+        self.policy_bn = nn.BatchNorm2d(2)
+        self.policy_fc = nn.Linear(2 * 8 * 8, 4096) 
+        
+        self.value_conv = nn.Conv2d(128, 1, kernel_size=1)
+        self.value_bn = nn.BatchNorm2d(1)
+        self.value_fc1 = nn.Linear(8 * 8, 256)
+        self.value_fc2 = nn.Linear(256, 1)
+
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.relu(self.batch_one(self.conv_one(x)))
+
+        x = self.res_stack(x)
+
+        p = self.relu(self.policy_bn(self.policy_conv(x)))
+        p = p.view(-1, 2 * 8 * 8)
+        policy = self.policy_fc(p)
+
+        v = self.relu(self.value_bn(self.value_conv(x)))
+        v = v.view(-1, 8 * 8)
+        v = self.relu(self.value_fc1(v))
+        value = torch.tanh(self.value_fc2(v))
+
+        return policy, value
 
 class ReplayMemory:
     def __init__(self, capacity):
