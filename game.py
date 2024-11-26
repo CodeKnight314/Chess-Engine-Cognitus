6b9bc3632820cfc517d104cbdb40618fe759c687
CodeKnight314 <rtang24@nmhschool.org>
2024-11-26 00:47:32 +0000

    Modifying game options

--- game.py
+++ game.py
@@ -5,8 +5,9 @@ from engine import find_best_move
 from board import Board
 from configs import *  # Import constants from configs.py
 import torch
-from train import get_state
 import os
+import time
+import chess.pgn
 
 class Game:
     def __init__(self):
@@ -27,13 +28,32 @@ class Game:
 
     def run(self):
         running = True
+        os.makedirs("Games_As_PGN/", exist_ok=True)
+        filename = os.path.join("Games_As_PGN",f"{time.time()}-game.pgn")
+        
+        game = chess.pgn.Game()
+        node = game
+        
+        if "renatus_vs_human":
+            user_turn = chess.BLACK 
+            comp_turn = chess.WHITE
+        elif "human_vs_renatus": 
+            user_turn = chess.WHITE 
+            comp_turn = chess.BLACK
+        else:
+            user_turn = None 
+            comp_turn = None
+        
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-                elif event.type == pygame.MOUSEBUTTONDOWN and self.board.turn == chess.WHITE:
+                elif event.type == pygame.MOUSEBUTTONDOWN and self.board.turn == user_turn:
                     # Only handle mouse clicks when it's human's turn in human_vs_renatus
-                    self.handle_mouse_click()
+                    usr_move = self.handle_mouse_click()
+                    if usr_move:
+                        node = node.add_variation(usr_move)
+                        print(game, file=open(filename, "w"), end="\n\n")
 
             # Drawing board and pieces
             self.screen.fill((0, 0, 0))  # Clear the screen
@@ -41,14 +61,27 @@ class Game:
             pygame.display.flip()
             self.clock.tick(60)  # Limit frame rate
 
-            # Handle Renatus move when it is Renatus's turn
-            if self.game_mode == "renatus_vs_renatus" and not self.board.is_game_over():
-                if self.board.turn == chess.WHITE or self.board.turn == chess.BLACK:
-                    pygame.time.delay(500)  # Delay to make moves readable
-                    self.renatus_move()
-            elif self.game_mode == "human_vs_renatus" and self.board.turn == chess.BLACK and not self.board.is_game_over():
-                pygame.time.delay(500)  # Delay to make Renatus move visually readable
-                self.renatus_move()
+            if not self.board.is_game_over():
+                if self.game_mode == "renatus_vs_renatus":
+                    if self.board.turn == chess.WHITE or self.board.turn == chess.BLACK:
+                        pygame.time.delay(500)
+                        comp_move = self.renatus_move()
+                        node = node.add_variation(comp_move)
+                        print(game, file=open(filename, "w"), end="\n\n")
+
+                elif self.game_mode == "human_vs_renatus":
+                    if self.board.turn == comp_turn:
+                        pygame.time.delay(500)
+                        comp_move = self.renatus_move()
+                        node = node.add_variation(comp_move)
+                        print(game, file=open(filename, "w"), end="\n\n")
+
+                elif self.game_mode == "renatus_vs_human":
+                    if self.board.turn == comp_turn:
+                        pygame.time.delay(500)
+                        comp_move = self.renatus_move()
+                        node = node.add_variation(comp_move)
+                        print(game, file=open(filename, "w"), end="\n\n")
 
         pygame.quit()
 
@@ -72,15 +105,17 @@ class Game:
                 self.board.push(move)
                 self.selected_square = None
                 self.legal_moves = []
+                return move
             else:
                 self.selected_square = None  # Deselect if invalid move
                 self.legal_moves = []
 
     def renatus_move(self):
         """Makes a move using the Renatus model."""
-        print(f"\nThinking move for {"white" if self.board.turn else "Black"}")
-        best_move = find_best_move(self.board, 4)
+        print(f"\nThinking move for {"White" if self.board.turn else "Black"}")
+        best_move = find_best_move(self.board, 4, time_limit=20)
         self.board.push(best_move)
+        return best_move
 
 if __name__ == "__main__":
     game = Game()
